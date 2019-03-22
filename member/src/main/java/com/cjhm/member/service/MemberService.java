2d313c43c280c6ee366b5c19ea71e41fb359a745
close852 <close852@naver.com>
2019-03-22 18:08:58 +0900

    spring-member login 페이지

---
+++ member/src/main/java/com/cjhm/member/service/MemberService.java
@@ -0,0 +1,37 @@
+package com.cjhm.member.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.cjhm.member.entity.User;
+import com.cjhm.member.repository.MemberRepository;
+
+@Service
+public class MemberService { 
+
+	MemberRepository memberRepository;
+
+	Logger logger = LoggerFactory.getLogger(MemberService.class);
+
+	public MemberService(MemberRepository memberRepository) {
+		this.memberRepository = memberRepository;
+	}
+
+	public User login(String email, String password) {
+
+		logger.info("login info : " + email + " : " + password);
+		User u = memberRepository.findByEmail(email);
+		logger.info("create user : " + u);
+		String encriptPassword = encript(password);
+		if (u != null && u.getPassword().equals(encriptPassword)) {
+			return u;
+		}
+		return null;
+	}
+
+	private String encript(String password) {
+		return password;
+	}
+
+}
