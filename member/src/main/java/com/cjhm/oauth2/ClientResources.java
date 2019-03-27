c03e2d59da770e398dd4e402146df3123986ee04
close852 <close852@naver.com>
2019-03-27 10:15:29 +0900

    Spring-oauth social 연동 준비

---
+++ member/src/main/java/com/cjhm/oauth2/ClientResources.java
@@ -0,0 +1,22 @@
+package com.cjhm.oauth2;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.boot.context.properties.NestedConfigurationProperty;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+
+public class ClientResources {
+
+	@NestedConfigurationProperty
+	private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
+	
+	@NestedConfigurationProperty
+	private OAuth2ResourceServerProperties resource = new OAuth2ResourceServerProperties();
+	
+	public AuthorizationCodeResourceDetails getClient() {
+		return client;
+	}
+	
+	public OAuth2ResourceServerProperties getResource() {
+		return resource;
+	}
+}
